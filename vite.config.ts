d346dde3f6d4543c99ad29c787d90e8f83e89847
Gilvan Sousa <74104232+GilvanS@users.noreply.github.com>
2025-11-04 20:59:03 -0300

    feat: Update deployment configuration
    Updates the `homepage` and `base` paths in `package.json` and `vite.config.ts` respectively to reflect the new repository name 'portifolioPessoal'. Also updates the `deploy` script to use `gh-pages` directly for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portifolioPessoal/',
   plugins: [react()],
-  // IMPORTANT: Replace 'portfolio' with the name of your GitHub repository.
-  base: '/portfolio/',
 })
